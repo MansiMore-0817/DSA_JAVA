1e701332a3c76ab1d8ab81baca28e1adcf097133
Mansi More <moremansi0817@gmail.com>
2025-12-24 02:08:05 +0530

    Advanced patterns solving

---
+++ patterns/solidRhombus.java
@@ -0,0 +1,22 @@
+public class solidRhombus {
+        
+    public static void solidRhombus(int n){
+
+
+        for(int i = 1; i<= n; i++){
+            // spaces
+            for(int j = n-i; j> 0; j--){
+                System.out.print(" ");
+            }
+            // stars
+            for(int j = 1; j<=n; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        solidRhombus(7);
+    }
+}
