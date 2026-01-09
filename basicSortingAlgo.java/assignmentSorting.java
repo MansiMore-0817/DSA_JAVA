0a9d036be47d4d9f3a1e8b37030d5fe133088eb0
Mansi More <moremansi0817@gmail.com>
2026-01-10 01:12:30 +0530

    assignment done on sorting

--- basicSortingAlgo.java/assignmentSorting.java
+++ basicSortingAlgo.java/assignmentSorting.java
@@ -25,7 +25,7 @@ public class assignmentSorting {
         int n = arr.length;
         for(int i = 0; i < n-1; i++){
             int minIdx = i;
-            for(int j = i + 1; j < n; j++){
+            for(int j = i + 1; j < n; j++){''
                 if(arr[j] < arr[minIdx]){
                     minIdx = j;
                 
