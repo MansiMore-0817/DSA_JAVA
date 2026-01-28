65ab5fa1e6917089c83730408d1752c9d80ac7f7
Mansi More <moremansi0817@gmail.com>
2026-01-28 21:14:55 +0530

    fixed

--- strings/checkPalindrome.java
+++ strings/checkPalindrome.java
@@ -1,25 +1,22 @@
 public class checkPalindrome {
 
-        public static boolean isPalindrome(String str){
-            for(int i = 0; i < str.length()/2; i++){
-                int n = str.length();
-                if(str.charAt(i) != str.charAt(n-1-i)){
-                        // not a palindrome
-                        return false;
-                }
+    public static boolean isPalindrome(String str) {
+        for (int i = 0; i < str.length() / 2; i++) {
+            int n = str.length();
+            if (str.charAt(i) != str.charAt(n - 1 - i)) {
+                // not a palindrome
+                return false;
             }
-
-            return true;
         }
 
+        return true;
+    }
 
-        public static void main(String [] args){
-            String str = "racecar";
+    public static void main(String[] args) {
+        String str = "racecar";
 
-            System.out.println(isPalindrome(str));
+        System.out.println(isPalindrome(str));
 
-
-        }
-
+    }
 
 }
